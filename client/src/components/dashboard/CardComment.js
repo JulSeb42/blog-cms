304ece54645cf3676db6ce5805f3d04127c30aaa
Julien Sebag <julien.sebag@me.com>
2022-01-13 18:07:32 +0100

    Added alert delete comment

--- client/src/components/dashboard/CardComment.js
+++ client/src/components/dashboard/CardComment.js
@@ -1,5 +1,5 @@
 // Packages
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import Link from "../utils/LinkScroll"
 import axios from "axios"
@@ -9,6 +9,7 @@ import * as Variables from "../styles/Variables"
 import * as Font from "../styles/Font"
 import ButtonIcon, { IconsContainer } from "../ui/ButtonIcon"
 import TitleFlex from "../ui/TitleFlex"
+import Alert from "../ui/Alert"
 
 // Utils
 import convertDate from "../utils/convertDate"
@@ -30,44 +31,66 @@ const Title = styled.span`
 `
 
 function CardComment({ comment, ...props }) {
+    const [cardVisibility, setCardVisibility] = useState("grid")
+
     // Delete comment
     const handleDelete = e => {
         e.preventDefault()
 
         axios
             .delete(`/comments/comment/${comment._id}/delete`)
-            .then(() => window.location.reload(false))
+            .then(() => {
+                setIsVisible(true)
+                setCardVisibility("none")
+            })
             .catch(err => console.log(err))
     }
 
+    // Show / hide alert
+    const [isVisible, setIsVisible] = useState(false)
+
+    if (isVisible) {
+        setTimeout(() => {
+            setIsVisible(false)
+        }, 3000)
+    }
+
     return (
-        <Container>
-            <TitleFlex>
-                <Title>
-                    <Font.H5>{comment.poster}</Font.H5>
-                    <Font.Label as="small">
-                        {convertDate(comment.date)} at {comment.time}
-                    </Font.Label>
+        <>
+            <Container style={{ display: cardVisibility }}>
+                <TitleFlex>
+                    <Title>
+                        <Font.H5>{comment.poster}</Font.H5>
+                        <Font.Label as="small">
+                            {convertDate(comment.date)} at {comment.time}
+                        </Font.Label>
 
-                    <Font.Label as="small">
-                        Posted in{" "}
-                        <Link
-                            to={`/posts/${comment.post.category}/${comment.post.slug}`}
-                            target="_blank"
-                            rel="noreferrer noopener"
-                        >
-                            {comment.post.title}
-                        </Link>
-                    </Font.Label>
-                </Title>
+                        <Font.Label as="small">
+                            Posted in{" "}
+                            <Link
+                                to={`/posts/${comment.post.category}/${comment.post.slug}`}
+                                target="_blank"
+                                rel="noreferrer noopener"
+                            >
+                                {comment.post.title}
+                            </Link>
+                        </Font.Label>
+                    </Title>
 
-                <IconsContainer>
-                    <ButtonIcon icon="trash" onClick={handleDelete} />
-                </IconsContainer>
-            </TitleFlex>
+                    <IconsContainer>
+                        <ButtonIcon icon="trash" onClick={handleDelete} />
+                    </IconsContainer>
+                </TitleFlex>
 
-            <Font.P pre>{comment.body}</Font.P>
-        </Container>
+                <Font.P pre>{comment.body}</Font.P>
+            </Container>
+
+            <Alert
+                alertstyle="danger"
+                message="The comment has been deleted"
+                isVisible={isVisible}
+            />
+        </>
     )
 }
 
