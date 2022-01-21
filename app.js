04bb480c7573d1880850b65e6e63bf853fbd0396
Julien Sebag <julien.sebag@me.com>
2022-01-21 16:19:24 +0100

    Added Heroku deploy code

--- app.js
+++ app.js
@@ -37,6 +37,14 @@ app.use("/pages", pages)
 const comments = require("./routes/comments")
 app.use("/comments", comments)
 
+// Deploy Heroku
+const path = require("path")
+app.use(express.static(path.join(__dirname, "/client/build")))
+app.use((req, res) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/client/build/index.html")
+})
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app)
 
