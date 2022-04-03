0369a469724be37338d5271ba078be080ba76018
Julien <julien.sebag@me.com>
2022-04-03 23:10:40 +0200

    Clean files

--- utils/consts.js
+++
@@ -1,4 +0,0 @@
-const MONGO_URI =
-    process.env.MONGODB_URI || "mongodb://localhost/blog-cms"
-
-module.exports = MONGO_URI
--- utils/convertToEmail.js
+++
@@ -1,5 +0,0 @@
-const convertToEmail = str => {
-    return `${str.toString().toLowerCase().replace(" ", "-")}@email.com`
-}
-
-module.exports = convertToEmail
